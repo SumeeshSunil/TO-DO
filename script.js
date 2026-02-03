9ea4ec08f971c08f3a77b34408f867d6baee5485
SumeeshSunil <sumeeshsunil111@gmail.com>
2026-02-03 16:27:03 +0530

    All files are initialized

---
+++ script.js
@@ -0,0 +1,15 @@
+// Create a task Array
+let tasks = [];
+
+function addTask () {
+  let taskInput = document.getElementById("task");
+  let text = taskInput.value;
+  if ( text === ""){
+    alert("Please enter a task");
+    return;
+  }else{
+    tasks.push(text);
+    taskInput.value = "";
+    console.log(tasks);
+    }
+}
