6eae4d1ead1b055b245adb34049c7289d927416d
SumeeshSunil <sumeeshsunil111@gmail.com>
2026-02-05 11:45:02 +0530

    all files changed to add event listner

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
 // Create a task Array
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-function addTask () {
-  let taskInput = document.getElementById("task");
-  let text = taskInput.value;
-  if ( text === ""){
-    alert("Please enter a task");
+ 
+console.log(JSON.parse(localStorage.getItem("tasks")));
+
+
+const taskInput = document.getElementById("task");
+const addBtn = document.getElementById("addBtn");
+const clearBtn = document.getElementById("clearBtn");
+const list = document.getElementById("list");
+
+addBtn.addEventListener("click", addTask);
+
+clearBtn.addEventListener("click", () =>{
+  tasks = [];
+  localStorage.removeItem("tasks");
+  displayTasks();
+});
+
+function addTask() {
+  let text = taskInput.value.trim();
+
+  if (text === "") {
+    alert("Enter a task");
     return;
-  }else{
-    tasks.push(text);
-    taskInput.value = "";
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    let saved = localStorage.getItem("tasks");
-    console.log(saved);
-    }
+  }
+  tasks.push(text);
+  taskInput.value = "";
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  displayTasks();
+  console.log(tasks);
 }
 
+function displayTasks() {
+  const list = document.getElementById("list");
+  list.innerHTML = "";  
+
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "X";
+
+    removeBtn.addEventListener("click", () => removeTask(index));
+
+    li.appendChild(removeBtn);
+    list.appendChild(li);
+  });
+}
+
+function removeTask(index) {
+  tasks.splice(index, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  displayTasks();
+
+}
+displayTasks();
